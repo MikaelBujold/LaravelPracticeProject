43d2080f5d2904421422d01440c5f0827593c6ce
BobbySystem <mikael.bujold.1@ens.etsmtl.ca>
2022-05-17 11:10:07 -0400

    First commit.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <iframe width="100%" height="480px" src="//www.youtube.com/embed/ImtZ5yENzgE" frameborder="0" allowfullscreen></iframe>
+        <div><strong>Laravel PHP Framework Tutorial - Full Course for Beginners (2019)</strong></div>
+        <div><strong>1,870,732 views  May 7, 2019</strong></div>
+        <div>desc...</div>
+            <div class="d-flex">
+                <div onclick="true" class="pe-4"><strong><img src="svg/likeButton.svg" style="height:24px;"> 26k</strong></div>
+                <div class="pe-4"><strong><img src="svg/dislikeButton.svg" style="height:24px;"> Dislike</strong></div>
+                <div class="pe-4"><strong><img src="svg/shareButton.svg" style="height:24px;"> Share</strong></div>
+                <div class="pe-4"><strong><img src="svg/downloadButton.svg" style="height:24px;"> Download</strong></div>
+                <div class="pe-4"><strong><img src="svg/supportButton.svg" style="height:24px;"> Thanks</strong></div>
+                <div class="pe-4"><strong><img src="svg/clipButton.svg" style="height:24px;"> Clip</strong></div>
+                <div class="pe-4"><strong><img src="svg/saveButton.svg" style="height:24px;"> Save</strong></div>
+                <div class="pe-4"><strong><img src="svg/moreButton.svg" style="height:24px;"></strong></div>
+            </div>
+        <div class="col-2"></div>
+    </div>
+</div>
+@endsection
