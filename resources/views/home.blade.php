b1e621027c1940084f492ff49e5c856a21636625
MikaelBujold <mikael.bujold.1@ens.etsmtl.ca>
2022-05-17 14:19:39 -0400

    Padding fixed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,22 @@
 <div class="container">
     <div class="row">
         <iframe width="100%" height="480px" src="//www.youtube.com/embed/ImtZ5yENzgE" frameborder="0" allowfullscreen></iframe>
-        <div><strong>Laravel PHP Framework Tutorial - Full Course for Beginners (2019)</strong></div>
-        <div><strong>1,870,732 views  May 7, 2019</strong></div>
-        <div>desc...</div>
-            <div class="d-flex">
-                <div class="pe-4"><strong><img src="svg/likeButton.svg" style="height:24px;"> 26k</strong></div>
-                <div class="pe-4"><strong><img src="svg/dislikeButton.svg" style="height:24px;"> Dislike</strong></div>
-                <div class="pe-4"><strong><img src="svg/shareButton.svg" style="height:24px;"> Share</strong></div>
-                <div class="pe-4"><strong><img src="svg/downloadButton.svg" style="height:24px;"> Download</strong></div>
-                <div class="pe-4"><strong><img src="svg/supportButton.svg" style="height:24px;"> Thanks</strong></div>
-                <div class="pe-4"><strong><img src="svg/clipButton.svg" style="height:24px;"> Clip</strong></div>
-                <div class="pe-4"><strong><img src="svg/saveButton.svg" style="height:24px;"> Save</strong></div>
-                <div class="pe-4"><strong><img src="svg/moreButton.svg" style="height:24px;"></strong></div>
-            </div>
+        <div style="font-size:18px; padding-top: 20px"><strong>Laravel PHP Framework Tutorial - Full Course for Beginners (2019)</strong></div>
+        <div class="d-flex" style="padding-top: 8px">
+            <div style="font-size:14px;font-weight: bold">1,870,732 views  May 7, 2019</div>
+            <div style="color:grey; padding-left: 6px">desc...</div>
+        </div>
+
+        <div class="d-flex" style="font-size:14px; padding-top: 8px">
+            <div class="info pe-4"><strong><img src="svg/likeButton.svg" style="height:24px;"> 26k</strong></div>
+            <div class="info pe-4"><strong><img src="svg/dislikeButton.svg" style="height:24px;"> Dislike</strong></div>
+            <div class="info pe-4"><strong><img src="svg/shareButton.svg" style="height:24px;"> Share</strong></div>
+            <div class="info pe-4"><strong><img src="svg/downloadButton.svg" style="height:24px;"> Download</strong></div>
+            <div class="info pe-4"><strong><img src="svg/supportButton.svg" style="height:24px;"> Thanks</strong></div>
+            <div class="info pe-4"><strong><img src="svg/clipButton.svg" style="height:24px;"> Clip</strong></div>
+            <div class="info pe-4"><strong><img src="svg/saveButton.svg" style="height:24px;"> Save</strong></div>
+            <div class="info pe-4"><strong><img src="svg/moreButton.svg" style="height:24px;"></strong></div>
+        </div>
         <div class="col-2"></div>
     </div>
 </div>
