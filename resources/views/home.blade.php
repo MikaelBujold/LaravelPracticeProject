083af802feb9b8a5428d7a3dedc2f9e707811d5c
MikaelBujold <mikael.bujold.1@ens.etsmtl.ca>
2022-05-17 15:05:27 -0400

    Added hover text.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,14 +11,14 @@
         </div>
 
         <div class="d-flex" style="font-size:14px; padding-top: 8px">
-            <div class="info pe-4"><strong><img src="svg/likeButton.svg" style="height:24px;"> 26k</strong></div>
-            <div class="info pe-4"><strong><img src="svg/dislikeButton.svg" style="height:24px;"> Dislike</strong></div>
-            <div class="info pe-4"><strong><img src="svg/shareButton.svg" style="height:24px;"> Share</strong></div>
-            <div class="info pe-4"><strong><img src="svg/downloadButton.svg" style="height:24px;"> Download</strong></div>
-            <div class="info pe-4"><strong><img src="svg/supportButton.svg" style="height:24px;"> Thanks</strong></div>
-            <div class="info pe-4"><strong><img src="svg/clipButton.svg" style="height:24px;"> Clip</strong></div>
-            <div class="info pe-4"><strong><img src="svg/saveButton.svg" style="height:24px;"> Save</strong></div>
-            <div class="info pe-4"><strong><img src="svg/moreButton.svg" style="height:24px;"></strong></div>
+            <div onclick="" title="I like this" class="info pe-4"><strong><img src="svg/likeButton.svg" style="height:24px;"> 26k</strong></div>
+            <div onclick="" title="I dislike this" class="info pe-4"><strong><img src="svg/dislikeButton.svg" style="height:24px;"> Dislike</strong></div>
+            <div onclick="" title="Share" class="info pe-4"><strong><img src="svg/shareButton.svg" style="height:24px;"> Share</strong></div>
+            <div onclick="" title="Download" class="info pe-4"><strong><img src="svg/downloadButton.svg" style="height:24px;"> Download</strong></div>
+            <div onclick="" title="Show support with Super Thanks" class="info pe-4"><strong><img src="svg/supportButton.svg" style="height:24px;"> Thanks</strong></div>
+            <div onclick="" title="Clip" class="info pe-4"><strong><img src="svg/clipButton.svg" style="height:24px;"> Clip</strong></div>
+            <div onclick="" title="Save" class="info pe-4"><strong><img src="svg/saveButton.svg" style="height:24px;"> Save</strong></div>
+            <div onclick="" class="info pe-4"><strong><img src="svg/moreButton.svg" style="height:24px;"></strong></div>
         </div>
         <div class="col-2"></div>
     </div>
