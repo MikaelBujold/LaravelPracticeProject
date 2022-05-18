71ffeb0daca29eb539c8013417e33e7237b91d4d
MikaelBujold <mikael.bujold.1@ens.etsmtl.ca>
2022-05-18 12:04:26 -0400

    Full commit blog page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,17 +10,35 @@
             <div style="color:grey; padding-left: 6px">desc...</div>
         </div>
 
-        <div class="d-flex" style="font-size:14px; padding-top: 8px">
-            <div onclick="" title="I like this" class="info pe-4"><strong><img src="svg/likeButton.svg" style="height:24px;"> 26k</strong></div>
-            <div onclick="" title="I dislike this" class="info pe-4"><strong><img src="svg/dislikeButton.svg" style="height:24px;"> Dislike</strong></div>
-            <div onclick="" title="Share" class="info pe-4"><strong><img src="svg/shareButton.svg" style="height:24px;"> Share</strong></div>
-            <div onclick="" title="Download" class="info pe-4"><strong><img src="svg/downloadButton.svg" style="height:24px;"> Download</strong></div>
-            <div onclick="" title="Show support with Super Thanks" class="info pe-4"><strong><img src="svg/supportButton.svg" style="height:24px;"> Thanks</strong></div>
-            <div onclick="" title="Clip" class="info pe-4"><strong><img src="svg/clipButton.svg" style="height:24px;"> Clip</strong></div>
-            <div onclick="" title="Save" class="info pe-4"><strong><img src="svg/saveButton.svg" style="height:24px;"> Save</strong></div>
-            <div onclick="" class="info pe-4"><strong><img src="svg/moreButton.svg" style="height:24px;"></strong></div>
+        <div class="d-flex" style="font-size:14px; padding-top: 8px; ">
+            <div onclick="" style="cursor: pointer; padding-right: 20px" title="I like this" class="info"><strong><img src="svg/likeButton.svg" style="height:24px;"> 26k</strong></div>
+            <div onclick="" style="cursor: pointer; padding-right: 20px" title="I dislike this" class="info"><strong><img src="svg/dislikeButton.svg" style="height:24px;"> Dislike</strong></div>
+            <div onclick="" style="cursor: pointer; padding-right: 20px" title="Share" class="info"><strong><img src="svg/shareButton.svg" style="height:24px;"> Share</strong></div>
+            <div onclick="" style="cursor: pointer; padding-right: 20px" title="Download" class="info"><strong><img src="svg/downloadButton.svg" style="height:24px;"> Download</strong></div>
+            <div onclick="" style="cursor: pointer; padding-right: 20px" title="Show support with Super Thanks" class="info"><strong><img src="svg/supportButton.svg" style="height:24px;"> Thanks</strong></div>
+            <div onclick="" style="cursor: pointer; padding-right: 20px" title="Clip" class="info"><strong><img src="svg/clipButton.svg" style="height:24px;"> Clip</strong></div>
+            <div onclick="" style="cursor: pointer; padding-right: 20px" title="Save" class="info"><strong><img src="svg/saveButton.svg" style="height:24px;"> Save</strong></div>
+            <div onclick="" style="cursor: pointer; padding-right: 20px" class="info"><strong><img src="svg/moreButton.svg" style="height:24px;"></strong></div>
         </div>
-        <div class="col-2"></div>
+        <div class="d-flex" style="font-size:14px; padding-top: 8px">
+            <div class="d-flex" style="font-size:14px; padding-top: 8px">
+                <div><img style="border-radius: 50%" src="https://yt3.ggpht.com/ytc/AKedOLRR2uNiXJiFH-XRmtGgkdICxTuDJxCPJidKFRNCNg=s48-c-k-c0x00ffffff-no-rj" alt=""></div>
+                <div style="padding-left: 8px">
+                    <div>Channel</div>
+                    <div>5.59M subscribers</div>
+                </div>
+                <div>Verified icon</div>
+                <p>JOIN button</p>
+                <p style="text-align:right;">SUBSCRIBE button</p>
+            </div>
+            <div class="d-flex" style="font-size:14px; padding-top: 8px;">
+                <div>Comments</div>
+                <div>2.5k</div>
+                <div>|</div>
+                <div>Icon</div>
+                <div>Comment</div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
