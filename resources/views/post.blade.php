c2c1125561dd0943d86176e35634880fdac08964
MikaelBujold <mikael.bujold.1@ens.etsmtl.ca>
2022-05-20 10:08:16 -0400

    Database only allow certain infos to be changed.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <article>
-        <h1>{{ $post->title }}</h1>
+        <h1>{!! $post->title !!}</h1>
 
         <div>
             {!! $post->body !!}
