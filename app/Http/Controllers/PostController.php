2f8a34e8fb4eeb6163b42c9e4bcce43c64832a6a
MikaelBujold <mikael.bujold.1@ens.etsmtl.ca>
2022-05-24 16:43:43 -0400

    User registering

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,8 +10,8 @@ class PostController extends Controller
     {
         return view('posts.index', [
             'posts' => Post::latest()->filter(
-                request(['search', 'category', 'author'])
-            )->paginate(18)->withQueryString()
+                        request(['search', 'category', 'author'])
+                    )->paginate(18)->withQueryString()
         ]);
     }
 
