c7c06e6da516af67f3c46c992aaefbe811d3ffd4
MikaelBujold <mikael.bujold.1@ens.etsmtl.ca>
2022-05-27 17:30:31 -0400

    Test for successful and failed login

--- tests/cypress/integration/auth_spec.js
+++ tests/cypress/integration/auth_spec.js
@@ -1,7 +1,27 @@
 describe('Authentication', ()=> {
-    it('signs a user in', ()=> {
+    it('provides feedback for invalid login credentials', ()=> {
         cy.refreshDatabase();
 
         cy.visit('/login');
+
+        cy.get('#email').type('foo@example.com');
+        cy.get('#password').type('password');
+        cy.contains('button', 'Log In').click();
+
+        cy.contains('Your provided credentials could not be verified.');
+    });
+
+    it('provides feedback for valid login credentials', ()=> {
+        cy.refreshDatabase();
+
+        cy.create("App\\Models\\User", {email: 'joe@example.com', password: 'password'});
+
+        cy.visit('/login');
+
+        cy.get('#email').type('joe@example.com');
+        cy.get('#password').type('password');
+        cy.contains('button', 'Log In').click();
+
+        cy.assertRedirect('/');
     });
 });
